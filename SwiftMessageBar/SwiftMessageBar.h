c1f167cf7b556cb7083f310ba1ae195edaf2047e
Jan Gorman <gorman.jan@gmail.com>
2015-06-10 20:06:06 +0200

    Initial commit

---
+++ SwiftMessageBar/SwiftMessageBar.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMessageBar.h
+//  SwiftMessageBar
+//
+//  Created by Jan Gorman on 10/06/15.
+//  Copyright (c) 2015 Schnaub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMessageBar.
+FOUNDATION_EXPORT double SwiftMessageBarVersionNumber;
+
+//! Project version string for SwiftMessageBar.
+FOUNDATION_EXPORT const unsigned char SwiftMessageBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMessageBar/PublicHeader.h>
+
+
